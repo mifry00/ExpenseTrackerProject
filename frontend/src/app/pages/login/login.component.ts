6630fe6d113b9a90bdb45b527d26d2b67a5eaa5c
Mille Frydeborg <millefrydeborg@Milles-MBP.home>
2025-04-21 18:51:35 +0200

    Add Angular frontend

---
+++ frontend/src/app/pages/login/login.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  standalone: true, 
+  imports: [CommonModule, FormsModule, RouterModule], 
+  templateUrl: './login.component.html',
+})
+export class LoginComponent {
+  email: string = '';
+  password: string = '';
+  error: string = '';
+
+  constructor(private http: HttpClient, private router: Router) {}
+
+  login() {
+    const payload = {
+      email: this.email,
+      passwordHash: this.password
+    };
+
+    this.http.post<any>('http://localhost:5068/api/user/login', payload).subscribe({
+      next: (res) => {
+        localStorage.setItem('userId', res.userId);
+        localStorage.setItem('isAdmin', res.isAdmin);
+        this.router.navigate([res.isAdmin ? '/admin' : '/expenses']);
+      },
+      error: () => {
+        this.error = 'Login failed. Check your email and password.';
+      }
+    });
+  }
+}
